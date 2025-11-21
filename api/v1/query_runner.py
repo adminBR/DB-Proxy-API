d36de0b4c4914317fceb1ad92498ddd941df1c95
adminBR <luis.cardosodev@gmail.com>
2025-11-21 16:21:47 -0300

    Enhance query runner API to support database name and improve error handling Fixing error on getting sql data from dictionary instead of mappins

--- api/v1/query_runner.py
+++ api/v1/query_runner.py
@@ -4,13 +4,13 @@ from util.db_connectors import get_connection
 from sqlalchemy import text
 from time import time
 
-
 router = APIRouter()
 
 
 class queryRequest(BaseModel):
     query: str
     database: str
+    database_name: str
 
 
 class queryResponse(BaseModel):
@@ -21,24 +21,26 @@ class queryResponse(BaseModel):
 
 @router.post("/run_query", response_model=queryResponse)
 def run_query_basic(request: queryRequest):
-    start_time = time()
+    if request.query.strip() == "":
+        raise HTTPException(status_code=400, detail="Query cannot be empty")
+
     try:
-        conn = get_connection(request.database)
-    except Exception as exc:
+        conn = get_connection(request.database, request.database_name)
+    except Exception as e:
         raise HTTPException(
-            status_code=500, detail=f"Failed to acquire connection: {exc}"
-        ) from exc
+            status_code=400, detail=f"Failed to acquire connection: {e}"
+        )
 
     try:
         with conn.connect() as connection:
+            start_time = time()
+
             result = connection.execute(text(request.query))
-            rows = [dict(row) for row in result]
             headers = list(result.keys())
+            rows = [dict(row) for row in result.mappings()]
             time_taken = time() - start_time
 
             response = queryResponse(time_taken=time_taken, headers=headers, data=rows)
             return response
-    except Exception as exc:
-        raise HTTPException(
-            status_code=500, detail=f"Query execution failed: {exc}"
-        ) from exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Query execution failed: {e}")
--- util/db_connectors.py
+++ util/db_connectors.py
@@ -16,10 +16,10 @@ def get_oracle_connection(env="prod"):
         raise Exception(e)
 
 
-def get_postgres_connection():
+def get_postgres_connection(database_name: str):
     try:
         engine = create_engine(
-            f'postgresql+psycopg2://{POSTGRES_DB_PARAMS["user"]}:{POSTGRES_DB_PARAMS["password"]}@{POSTGRES_DB_PARAMS["host"]}:{POSTGRES_DB_PARAMS["port"]}/{POSTGRES_DB_PARAMS["db_name"]}'
+            f'postgresql+psycopg2://{POSTGRES_DB_PARAMS["user"]}:{POSTGRES_DB_PARAMS["password"]}@{POSTGRES_DB_PARAMS["host"]}:{POSTGRES_DB_PARAMS["port"]}/{database_name}'
         )
         return engine
     except Exception as e:
@@ -27,10 +27,17 @@ def get_postgres_connection():
         raise Exception(e)
 
 
-def get_connection(database: str, env="prod"):
+def get_connection(database: str, database_name: str, env="prod"):
     if database.lower() == "oracle":
         return get_oracle_connection(env)
     elif database.lower() == "postgres":
-        return get_postgres_connection()
+        return get_postgres_connection(database_name)
     else:
-        raise ValueError("Unsupported database type")
+        raise ValueError(
+            f"Unsupported database type '{database}', please use one of the active databases: {get_active_databases()}"
+        )
+
+
+# placeholder for future organization of active databases
+def get_active_databases():
+    return {"oracle": "Oracle DB Connection", "postgres": "Postgres DB Connection"}
