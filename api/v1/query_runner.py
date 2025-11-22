aa53503ff347f32af0f7a9217d5c88cf6673f930
adminBR <luis.cardosodev@gmail.com>
2025-11-22 13:28:01 -0300

    Refactor query execution to support async handling with asyncio instead of async db libraries for better management. Added caching to the async endpoint

--- api/v1/query_runner.py
+++ api/v1/query_runner.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from util.db_connectors import get_connection, get_connection_async
+from util.db_connectors import get_db_engine, get_redis_client
 from sqlalchemy import text
+import asyncio
 from time import time
+from typing import Optional
+import json
+from pydantic import BaseModel, Field
 
 
 router = APIRouter()
+
+r = get_redis_client()
 
 
 class QueryRequest(BaseModel):
@@ -19,12 +25,19 @@ class QueryRequest(BaseModel):
     query: str
     database: str
     database_name: str
+    cache: Optional[bool] = False
+    cache_duration_seconds: Optional[int] = Field(300, ge=10, le=3600)
 
 
 class QueryResponse(BaseModel):
     time_taken: float
     headers: list[str]
     data: list[dict]
+
+
+async def run_blocking_query(engine, query):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, lambda: engine.execute(query))
 
 
 # Endpoint for query execution with improved I/O handling
@@ -34,22 +47,46 @@ async def run_query_async(request: QueryRequest):
         raise HTTPException(status_code=400, detail="Query cannot be empty")
 
     try:
-        conn = await get_connection_async(request.database, request.database_name)
+        engine = get_db_engine(request.database, request.database_name)
     except Exception as e:
         raise HTTPException(
             status_code=400, detail=f"Failed to acquire connection: {e}"
         )
 
-    try:
-        start_time = time()
-        async with conn.connect() as connection:
-            result = await connection.execute(text(request.query))
-            headers = list(result.mappings().keys())
-            rows = [dict(row) for row in result.mappings().all()]
-            time_taken = time() - start_time
+    if request.cache:
+        cached_result = r.get(request.query)
+        if cached_result and type(cached_result) == bytes:
+            cached_data = json.loads(cached_result)
+            return QueryResponse(
+                time_taken=0.0, headers=cached_data["headers"], data=cached_data["data"]
+            )
 
-            response = QueryResponse(time_taken=time_taken, headers=headers, data=rows)
-            return response
+    try:
+
+        def execute_query():
+            with engine.connect() as connection:
+                result = connection.execute(text(request.query))
+                rows = [dict(row) for row in result.mappings().all()]
+                headers = list(rows[0].keys()) if rows else []
+                return rows, headers
+
+        loop = asyncio.get_running_loop()
+
+        start_time = time()
+        rows, headers = await loop.run_in_executor(None, execute_query)
+        time_taken = time() - start_time
+
+        if request.cache:
+            r.set(
+                request.query,
+                json.dumps(
+                    {"headers": headers, "data": rows},
+                    default=str,
+                ),
+                ex=request.cache_duration_seconds,
+            )
+
+        return QueryResponse(time_taken=time_taken, headers=headers, data=rows)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Query execution failed: {e}")
 
@@ -60,19 +97,37 @@ def run_query_basic(request: QueryRequest):
         raise HTTPException(status_code=400, detail="Query cannot be empty")
 
     try:
-        conn = get_connection(request.database, request.database_name)
+        engine = get_db_engine(request.database, request.database_name)
     except Exception as e:
         raise HTTPException(
             status_code=400, detail=f"Failed to acquire connection: {e}"
         )
 
+    if request.cache:
+        cached_result = r.get(request.query)
+        if cached_result and type(cached_result) == bytes:
+            cached_data = json.loads(cached_result)
+            return QueryResponse(
+                time_taken=0.0, headers=cached_data["headers"], data=cached_data["data"]
+            )
+
     try:
         start_time = time()
-        with conn.connect() as connection:
+        with engine.connect() as connection:
             result = connection.execute(text(request.query))
-            headers = list(result.keys())
-            rows = [dict(row) for row in result.mappings()]
+            rows = [dict(row) for row in result.mappings().all()]
+            headers = list(rows[0].keys()) if rows else []
             time_taken = time() - start_time
+
+            if request.cache:
+                r.set(
+                    request.query,
+                    json.dumps(
+                        {"headers": headers, "data": rows},
+                        default=str,
+                    ),
+                    ex=request.cache_duration_seconds,
+                )
 
             response = QueryResponse(time_taken=time_taken, headers=headers, data=rows)
             return response
--- util/db_connectors.py
+++ util/db_connectors.py
@@ -1,25 +1,18 @@
 from sqlalchemy import create_engine
 from config import ORACLE_DB_PARAMS_PROD, ORACLE_DB_PARAMS_TEST, POSTGRES_DB_PARAMS
 from sqlalchemy.ext.asyncio import create_async_engine
+import redis
 
 
-def get_oracle_connection(env="prod"):
+def get_oracle_engine(env="prod"):
     params = ORACLE_DB_PARAMS_PROD if env == "prod" else ORACLE_DB_PARAMS_TEST
     conn_str = (
-        f"oracle+cx_oracle://{params['user']}:{params['password']}@{params['dsn']}"
+        f"oracle+oracledb://{params['user']}:{params['password']}@{params['dsn']}"
     )
     return create_engine(conn_str)
 
 
-async def get_oracle_connection_async(env="prod"):
-    params = ORACLE_DB_PARAMS_PROD if env == "prod" else ORACLE_DB_PARAMS_TEST
-    conn_str = (
-        f"oracle+asyncoracledb://{params['user']}:{params['password']}@{params['dsn']}"
-    )
-    return create_async_engine(conn_str)
-
-
-def get_postgres_connection(database_name: str):
+def get_postgres_engine(database_name: str):
     conn_str = (
         f"postgresql+psycopg2://{POSTGRES_DB_PARAMS['user']}:"
         f"{POSTGRES_DB_PARAMS['password']}@"
@@ -29,31 +22,15 @@ def get_postgres_connection(database_name: str):
     return create_engine(conn_str)
 
 
-async def get_postgres_connection_async(database_name: str):
-    conn_str = (
-        f"postgresql+asyncpg://{POSTGRES_DB_PARAMS['user']}:"
-        f"{POSTGRES_DB_PARAMS['password']}@"
-        f"{POSTGRES_DB_PARAMS['host']}:"
-        f"{POSTGRES_DB_PARAMS['port']}/{database_name}"
-    )
-    return create_async_engine(conn_str)
-
-
-def get_connection(database: str, database_name: str, env="prod"):
+def get_db_engine(database: str, database_name: str, env="prod"):
     if database.lower() == "oracle":
-        return get_oracle_connection(env)
+        return get_oracle_engine(env)
     if database.lower() == "postgres":
-        return get_postgres_connection(database_name)
+        return get_postgres_engine(database_name)
     raise ValueError(
         f"Unsupported database type '{database}', please use one of the active databases: 'oracle' or 'postgres'"
     )
 
 
-async def get_connection_async(database: str, database_name: str, env="prod"):
-    if database.lower() == "oracle":
-        return await get_oracle_connection_async(env)
-    if database.lower() == "postgres":
-        return await get_postgres_connection_async(database_name)
-    raise ValueError(
-        f"Unsupported database type '{database}', please use one of the active databases: 'oracle' or 'postgres'"
-    )
+def get_redis_client(host="192.168.1.16", port=6380, db=0):
+    return redis.Redis(host=host, port=port, db=db)
